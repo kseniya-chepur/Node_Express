8a7dce0d7362a8e8c0d6ec6a6275fd43a8ac9271
kseniya-chepur <64986545+kseniya-chepur@users.noreply.github.com>
2020-11-30 13:08:04 +0200

    Modified

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const hbs = require('express-handlebars');
 const path = require('path');
 const fs = require('fs');
 const registeredUsers = require('./registered_users.json');
+const filePath = path.join(process.cwd(), 'registered_users.json');
 let loginUser = false;
 
 const app = express();
-const filePath = path.join(process.cwd(), 'registered_users.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -31,20 +31,24 @@ app.get('/users', (req, res) => {
     } 
         res.redirect('/error');
 });
+
 app.get('/login', (req, res) => {
     res.render('login');
 });
 
 app.post('/registration', (req, res) => {
     const { userName, email, password } = req.body;
+
         registeredUsers.forEach((user) => {
         if (user.userName === userName || user.email === email) {
             errMsg = 'This email address or username belongs to a registered user';
             res.redirect('/error');           
         }
     });
+
     registeredUsers.push({ userName, email, password });
     const newUsers = JSON.stringify(registeredUsers);
+
     fs.writeFile(filePath, newUsers, err => {
         if (err) {
             throw err;
@@ -56,12 +60,14 @@ app.post('/registration', (req, res) => {
 
 app.post('/login', (req, res) => {
     const { email, password } = req.body;
+
     registeredUsers.forEach((user) => {
         if (user.email === email && user.password === password) {
             loginUser = true;
             res.redirect('/users');
         }
     });
+    
     errMsg = 'You entered wrong email or password during login';
     res.redirect('/error');
 });
